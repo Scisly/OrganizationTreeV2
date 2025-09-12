6dfd2d0f5621d04c8da14a91eadc1c741e8a8d3f
Karol Scisłowski <karol.scislo@gmail.com>
2025-09-12 12:17:17 +0200

    Add constants for layout configuration

---
+++ OrganizationTreeV2/services/utils/constants.ts
@@ -0,0 +1,17 @@
+/**
+ * Constants used throughout the OrganizationTreeV2 control
+ */
+
+// Layout constants
+export const FIXED_HEIGHT = 768;
+
+// Dagre layout constants
+export const LAYOUT_CONSTANTS = {
+  NODE_WIDTH: 220,
+  NODE_HEIGHT: 140,
+  RANK_SEPARATION: 80,
+  NODE_SEPARATION: 60,
+  EDGE_SEPARATION: 10,
+  MARGIN_X: 20,
+  MARGIN_Y: 20,
+} as const;
