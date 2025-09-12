22884d806ee42e10604f89aae3aae3db3a8a1ff7
Karol Scisłowski <karol.scislo@gmail.com>
2025-09-12 12:17:14 +0200

    Add barrel exports for panels

---
+++ OrganizationTreeV2/components/panels/index.ts
@@ -0,0 +1,6 @@
+export { ReactFlowContent } from "./ReactFlowContent";
+export { SurveyPanel } from "./SurveyPanel";
+export { DescriptionPanel } from "./DescriptionPanel";
+export type { ReactFlowContentProps } from "./ReactFlowContent";
+export type { SurveyPanelProps } from "./SurveyPanel";
+export type { DescriptionPanelProps } from "./DescriptionPanel";
